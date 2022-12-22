8f13d5a2158219b02236c5482ef7b80aa2b210f8
twilwa <twilwa@users.noreply.github.com>
2022-12-21 20:04:53 -0800

    generate merkle root hash

--- server/index.js
+++ server/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "89bd236fdbb74533384e39d2eeb951d510229c2c8e317510608db76c41c7c1a2";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
+  JSON.parse();
+  // TODO: prove that a name is in the list
   const isInTheList = false;
-  if(isInTheList) {
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
@@ -27,3 +26,5 @@ app.post('/gift', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
+
+//I am the verifier, i need to verify the name passed by client is in the merkle root
