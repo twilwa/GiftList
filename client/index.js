3d638c29743cb8951a133d31c731d6d5ae2a0889
twilwa <twilwa@users.noreply.github.com>
2022-12-23 15:34:20 -0800

    solution with cleaned up code.

--- client/index.js
+++ client/index.js
@@ -12,23 +12,13 @@ const {
 const serverUrl = "http://127.0.0.1:1225";
 
 const nameVar = process.argv[2];
-console.log(nameVar);
 const ourTree = new MerkleTree(niceList);
-
 const index = niceList.findIndex((n) => n === nameVar);
 const proof = ourTree.getProof(index);
-console.log(index);
-console.log(proof);
-console.log(ourTree.getRoot());
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list?
-  //we have to prove that the hash that we're providing corresponds to the hash of the branch of the hashed merkle tree that is our storage object
-  //this hash is a merkle tree that contains, at each node, a single name on the list (the hash of that name? idk if thats necessary)
-  //in rethinking the merkle tree and looking at the list, we'll do it by letter like in class
-  //if that tree contains all names..
-  //niceList is an array
-  console.log(nameVar);
+  // simple. provide a proof and a name to the server and have it check against the root. utility functions are all provided for us.
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     name: nameVar,
--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 
 const ourTree = new MerkleTree(niceList);
-console.log(ourTree.getRoot());
 
 const MERKLE_ROOT =
   "ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa";
@@ -20,7 +19,7 @@ app.post("/gift", (req, res) => {
   const body = req.body;
 
   // TODO: prove that a name is in the list
-  console.log(verifyProof(body.proof, body.name, MERKLE_ROOT));
+
   const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if (isInTheList) {
     res.send("You got a toy robot!");
